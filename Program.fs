de0570c39da8512e45c4be2058eb69c180131ffc
Jangbu Sherpa <jangbusherpa@Jangbus-MacBook-Air.local>
2023-11-24 10:00:03 -0500

    fourth commit: Added a function using list.sumBy

--- Program.fs
+++ Program.fs
@@ -20,3 +20,22 @@ type Activity =
     | Movie of Genre
     | Restaurant of Cuisine
     | LongDrive of int * float
+
+
+let calculateBudget (activity: Activity) =
+    match activity with
+    | BoardGame | Chill -> 0.0
+    | Movie genre ->
+        match genre with
+        | Action | Comedy | Drama -> 12.0
+        | SciFi -> 17.0
+    | Restaurant cuisine ->
+        match cuisine with
+        | Korean -> 70.0
+        | Turkish -> 65.0
+    | LongDrive (kilometers, fuelCharge) -> float kilometers * fuelCharge
+
+let calculateTotalBudget activities =
+    activities
+    |> List.sumBy calculateBudget
+
